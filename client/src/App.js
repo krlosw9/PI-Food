44561b1b42dc4c8c1149c80af66a0d1d56ef3edb
Carlos Waldo <krlosw9@gmail.com>
2023-01-18 20:25:23 -0500

    Se completo el landing page, se le dio contenido y estilos

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import { Switch, Route } from "react-router-dom";
 import Landing from "./view/Landing";
+import './App.css';
 
 function App() {
   return (
--- client/src/view/Landing.jsx
+++ client/src/view/Landing.jsx
@@ -1,10 +1,22 @@
-// import background from '../utils/background.png'
+import arrowRight from '../utils/arrowRight.png'
 import style from './Landing.module.css';
+import {Link} from 'react-router-dom'
 
 export default function Landing() {
   return(
     <main className={style.container}>
-      
+      <div className={style.card}>
+        <div className={style.cardTitle}>¡Wellcome!</div>
+        <div className={style.cardContent}>
+          On this site, you can find the best <span>recipes </span>.
+          In addition to the necessary <span>ingredients </span>and <span>instructions </span>to prepare them.
+        </div>
+        <Link to='/home'>
+          <button className={style.cardButton}>
+            See recipes <span><img src={arrowRight} alt="arrowRight" /> </span>
+          </button>
+        </Link>
+      </div>
     </main>
   )
 }
