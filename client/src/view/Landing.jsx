335c59b78ca5b51223c7318d82c9609d820f90ea
Carlos Waldo Rojas <krlosw9@gmail.com>
2023-01-19 21:17:23 -0500

    se modifico la imagen background de png a jpg, para tener un mejor peso kb en la imagen, se agrego el selector universal * con box-sizing y cambiado el width del background-image en 90% y un max-width

--- client/src/view/Landing.jsx
+++ client/src/view/Landing.jsx
@@ -1,4 +1,4 @@
-import arrowRight from '../utils/arrowRight.png'
+import arrowRight from '../utils/landing/arrowRight.png'
 import style from './Landing.module.css';
 import {Link} from 'react-router-dom'
 
@@ -8,7 +8,7 @@ export default function Landing() {
       <div className={style.card}>
         <h1 className={style.cardTitle}>Wellcome!</h1>
         <div className={style.cardContent}>
-          On this site, you can find the best <span>recipes </span>.
+          On this site, you can find the best <span>recipes. </span>
           In addition to the necessary <span>ingredients </span>and <span>instructions </span>to prepare them.
         </div>
         <Link to='/home'>
